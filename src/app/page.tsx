fe008d2897e52f169b9df60e3cd143542b989ac0
AryehSchoefer <aryeh.shmuel@googlemail.com>
2023-10-10 11:15:30 +0200

    Implemented infinite scrolling for seeing all 100 posts on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <main></main>;
+import InfiniteScrollPosts from "@/components/InfiniteScrollPosts";
+import { Flex } from "@chakra-ui/react";
+import { fetchPosts } from "@/lib/fetchPosts";
+
+export default async function Home() {
+  const initialPosts = await fetchPosts();
+  if (!initialPosts) {
+    throw new Error("Error while fetching posts");
+  }
+
+  return (
+    <main>
+      <Flex justify={"center"}>
+        <InfiniteScrollPosts initialPosts={initialPosts} />
+      </Flex>
+    </main>
+  );
 }
---
+++ src/components/InfiniteScrollPosts.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Flex, Spinner } from "@chakra-ui/react";
+import { useInView } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import { PostCard } from "./PostCard";
+import { Post, fetchPosts } from "@/lib/fetchPosts";
+
+function InfiniteScrollPosts({ initialPosts }: { initialPosts: Post[] }) {
+  // state variables used for pagination logic
+  const [posts, setPosts] = useState(initialPosts);
+  const [page, setPage] = useState(1);
+  // ref tracking for infinite scroll functionality:
+  // when the dom object of the ref comes in view, the useEffect
+  // is triggered which then loads more posts
+  const ref = useRef(null);
+  const lastPostInView = useInView(ref);
+
+  async function loadMorePosts() {
+    const next = page + 1;
+    const newPosts = await fetchPosts((next - 1) * 10);
+
+    if (!newPosts) {
+      throw new Error("Error while fetching posts");
+    }
+
+    setPage(next);
+    setPosts((prevPosts) => [...prevPosts, ...newPosts]);
+  }
+
+  useEffect(() => {
+    if (lastPostInView) {
+      loadMorePosts();
+    }
+  }, [lastPostInView]);
+
+  return (
+    <Flex direction="column" gap="10" align="center" pt="10" pb="10">
+      {posts.map((post) => (
+        <div key={post.id.toString()}>
+          <PostCard postId={post.id} title={post.title} body={post.body} />
+        </div>
+      ))}
+      {page <= 10 && <Spinner h="10" w="10" ref={ref} />}
+    </Flex>
+  );
+}
+
+export default InfiniteScrollPosts;
